cbffc39994f2111ad1caa3ccba98db56256a2bfb
Shawn Averkamp <shawnaverkamp@gmail.com>
2016-01-14 07:53:25 -0500

    update scheduler

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', hours=2)
 def timed_job():
+	os.system('genrebot.py')
     print('This job is run every two hours.')
 
 sched.start()
